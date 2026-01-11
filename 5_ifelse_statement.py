09abf0e8e54bf1758f7a79489ac1939640247a39
xervii <ahtaullahakmali@gmail.com>
2026-01-11 17:47:43 +0700

    Learning to make functions inneficiently, but still trying

---
+++ 5_ifelse_statement.py
@@ -0,0 +1,27 @@
+#Making some functions to check the user input
+
+name = input("What is your name?: ")
+age = int(input("What is your age?: "))
+is_online = True
+
+def name_check(name):
+    if name == "":
+        print("You didn't input your name!")
+    else:
+        print(f"hello, {name}")
+
+def age_check(age):
+    if age >= 18:
+        print("Access granted!")
+    else:
+        print("You must be 18+ to sign up!")
+
+def status_check(status):
+    if status:
+        print("User is online!")
+    else:
+        print("User is offline!")
+
+name_check(name)
+age_check(age)
+status_check(is_online)
