481e0b313a05a760b6c79776e712b01c81fbd8be
xervii <ahtaullahakmali@gmail.com>
2026-01-12 14:31:09 +0700

    Assigning a variable using a ternary operator

---
+++ 7_ternary_operator.py
@@ -0,0 +1,17 @@
+#Logical operators (and, or, not)
+
+name = input("What is your name?: ")
+age = int(input("What is your age?: "))
+is_vip = False
+is_senior = True if age >= 60 else False
+
+if name == "Felix" or is_vip:
+    print("Full access!")
+elif is_senior:
+    print("Special Access!")
+elif age >= 18:
+    print("Access granted as a regular member!")
+elif age <= 18:
+    print("No ENTRY! Age must be 18+ or become a VIP member")
+else:
+    print("Invalid somehow") #for troubleshootin shits
