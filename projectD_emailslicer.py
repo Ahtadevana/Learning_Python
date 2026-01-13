0886e078f5e2389bd719e630546ffd9dacac0b5b
xervii <ahtaullahakmali@gmail.com>
2026-01-13 11:09:52 +0700

    ""

--- projectD_emailslicer.py
+++ projectD_emailslicer.py
@@ -2,7 +2,7 @@
 
 email = input("Insert your email: ")
 
-if "@" in email:
+if "@" in email:    #if the character symbol @ is in var email, run this:
     slicer = email.index("@")
     print("Your username is " + email[:slicer])
     print("Your domain is " + email[slicer + 1:])
