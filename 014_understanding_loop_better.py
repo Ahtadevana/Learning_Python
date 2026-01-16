d610aaf8d075eadb26886d50c0793488bd9a0fc7
xervii <ahtaullahakmali@gmail.com>
2026-01-16 11:23:15 +0700

    Adding more for loops exercises! For loops is very hard to understand

--- 014_understanding_loop_better.py
+++ 014_understanding_loop_better.py
@@ -19,8 +19,60 @@ for i in range(10):
     i += 1
     result = number * i
     print(f"{number} x {i} = {result}")
+print(spaces)
 
-result = 0
+total_sum = 0
 for i in range(100):
-    result = result + (i + 1)
-print(result)
+    total_sum = total_sum + (i + 1)
+print(total_sum)
+print(spaces)
+
+for i in range(100):
+    words = input("Just input random words (q to quit): ")
+    if words.lower() == "q":
+        break
+    print(f"{words}")
+print(spaces)
+
+blocks = "[]"
+rows = 5
+columns = 2
+for i in range(rows):
+    print(blocks, end="")
+    for j in range(columns - 1):
+        print(blocks, end="")
+    print()
+print(spaces)
+    
+for i in range(rows):
+    print(blocks, end="")
+    for j in range(i):
+        print(blocks, end="")
+    print()
+print(spaces)
+
+for i in reversed(range(rows)):
+    print(blocks, end="")
+    for j in range(i):
+        print(blocks, end="")
+    print()
+print(spaces)
+
+name = input("Your name: ")
+for i in range(len(name)):
+    print(i, name[i])
+print(spaces)
+
+print('Password to pass is "Felixcute123"')
+for i in range(100):
+    password = input("Insert password: ")
+    if password == "Felixcute123":
+        break
+print(spaces)
+
+for i in range(1, 20, 3):
+    if i == 4:
+        continue    #for extra credits! uwu
+    if i == 16:
+        break
+    print(i)
