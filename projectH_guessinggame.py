347c174e59b7a01efa825dd6cbf7b118a06473c0
xervii <ahtaullahakmali@gmail.com>
2026-01-18 19:06:17 +0700

    Making a quiz game using 2d tuples and scoring system

--- projectH_guessinggame.py
+++ projectH_guessinggame.py
@@ -1,18 +1,45 @@
 #Making a quiz type game with list and tuples 
-choices = []    #user answer choices
-score = 0
-
 questions = (("1. What is the capital of the US?"), 
              ("2. What is the 2nd biggest planet in our solar system?"), 
-             ("3. What is the term CS in engineering stands for?"))
-options = [["A. Idaho", "B. Washington DC", "C. New Jersey", "D. Wyoming"], 
-           ["A. Venus", "B. Mercury", "C. Jupiter", "D. Saturn"], 
-           ["A. Computer Science", "B. Central Station", "C. Coming Soon", "D. Carbon steel"]]
-key_answer = (("b"), ("d"), ("a"))
+             ("3. What is the term CS in engineering stands for?"), 
+             ("4. Which animal has four legs?"))
+options = (("A. Idaho", "B. Washington DC", "C. New Jersey", "D. Wyoming"), 
+           ("A. Venus", "B. Mercury", "C. Jupiter", "D. Saturn"), 
+           ("A. Computer Science", "B. Central Station", "C. Coming Soon", "D. Carbon steel"),
+           ("A. Chicken", "B. Ostritch", "C. Cat", "D. T-rex"))
+key_answers = (("B"), ("D"), ("A"), ("D"))
 
-for question in questions:
+choices = []
+total_questions = 0
+score = 0
+
+for question in questions:  #prints questions
+    print("-----------------")
     print(question)
+    for option in options[total_questions]: #prints options
+        print(option)
 
-    choice = input("What's your answer: ")
+    choice = input("Write your answer: ").upper()   #user input
     choices.append(choice)
+    
+    if choice == key_answers[total_questions]:   #score check
+        print("correct!")
+        score += 1
+    else:
+        print("Incorrect!", end=" ")
+        print(f"Correct answer: {key_answers[total_questions]}")
 
+    total_questions += 1
+
+print("----- RESULT -----")
+print("Your guesses: ", end="")
+for choice in choices:
+    print(choice, end=" ")
+
+if score != total_questions:
+    print("\nThe correct answers: ", end="")
+    for answer in key_answers:
+        print(answer, end=" ")
+
+score *= 100 / total_questions
+print(f"\nYour total score is: {round(score)}%")
