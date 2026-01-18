75d8b3391b77d62f786ab3119c239adb9cded9cd
xervii <ahtaullahakmali@gmail.com>
2026-01-18 22:40:36 +0700

    Just getting to know of dicts

--- 017_dictionaries.py
+++ 017_dictionaries.py
@@ -1,18 +1,46 @@
+#dictionary simple principle {key:value}
+
 spaces = "--------------------"
 databank = {"feline": "cat",
             "poultry": "chicken",
             "vegetables": "broccoli",
             "fruit": "watermelon",}
 
-print(databank.items())
+data_items = databank.items()   #shows the whole key values within a dict
+print(data_items)
 print(spaces)
 
-for data in databank:
+for data in databank:   #displays dict one by one
     print(data)
 print(spaces)
 
-print(databank.keys())
+data_keys = databank.keys() #display only keys
+print(data_keys)
 print(spaces)
 
-print(databank.values())
+data_values = databank.values() #display only values
+print(data_values)
 print(spaces)
+
+databank.update({"feline":"lion"})  #rewriting/updating a certain key value within a dict
+print(databank)
+print(spaces)
+
+databank.pop("poultry") #removes a certain key value within a dict
+print(databank)
+print(spaces)
+
+databank.popitem()  #removes the latest key value added into the dict
+print(databank)
+print(spaces)
+
+databank.clear()    #entirely remove a dict to an empty one
+print(databank)
+print(spaces)
+
+#adds them back
+databank.update({"feline": "cat",
+                "poultry": "chicken",
+                "vegetables": "broccoli",
+                "fruit": "watermelon",})
+print(databank)
